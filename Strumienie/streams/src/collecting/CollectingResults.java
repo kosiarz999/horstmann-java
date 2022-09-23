6737743cbbd244ced89b54e623f3511c499b2534
Łukasz Kos <kos.lukasz.88@gmail.com>
2022-09-23 19:40:36 +0200

    Dodano gromadzenie wyników

--- Strumienie/streams/src/collecting/CollectingResults.java
+++ Strumienie/streams/src/collecting/CollectingResults.java
@@ -1,5 +1,75 @@
 package collecting;
 
-public class CollectingResults {
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+public class CollectingResults {
+    public static Stream<String> noVowels() throws IOException {
+        var contents = new String(Files.readAllBytes(
+                Paths.get("./lorem.txt")),
+                StandardCharsets.UTF_8);
+        List<String> wordList = List.of(contents.split("\\PL+"));
+        Stream<String> words = wordList.stream();
+        return words.map(s -> s.replaceAll("[aeiouAEIOU]", ""));
+    }
+
+    public static <T> void show(String label, Set<T> set) {
+        System.out.println(label + ": " + set.getClass().getName());
+        System.out.println("["
+                + set.stream().limit(10).map(Object::toString).collect(Collectors.joining(", "))
+                + "]");
+    }
+
+    public static void main(String[] args) throws IOException {
+        // drukuje "tablicę", iterator, od 0 do 9
+        Iterator<Integer> iter = Stream.iterate(0, n -> n + 1).limit(10).iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+
+        Object[] numbers = Stream.iterate(0, n -> n + 1).limit(10).toArray();
+        System.out.println("Tablica obiektów: " + numbers);
+
+        try {
+            var number = (Integer) numbers[0];
+            System.out.println("liczba: " + number);
+            System.out.println("Kolejna instrukcja wyrzuci wyjątek:");
+//            var numbers2 = (Integer[]) numbers; // Wyrzuca wyjątek
+        } catch (ClassCastException ex) {
+            System.out.println(ex);
+        }
+
+        Integer[] numbers3 = Stream.iterate(0, n -> n + 1)
+                .limit(10)
+                .toArray(Integer[]::new);
+        System.out.println("Tablica liczb całkowitych: " + numbers3);
+
+        Set<String> noVowelSet = noVowels().collect(Collectors.toSet());
+        show("noVowelSet", noVowelSet);
+
+        TreeSet<String> noVowelTreeSet = noVowels().collect(
+                Collectors.toCollection(TreeSet::new));
+        show("noVowelTreeSet", noVowelTreeSet);
+
+        String result = noVowels().limit(10).collect(Collectors.joining());
+        System.out.println("Konkatenacja: " + result);
+        result = noVowels().limit(10)
+                .collect(Collectors.joining(", "));
+        System.out.println("Konkatenacja z użyciem przecinków: " + result);
+
+        IntSummaryStatistics summary = noVowels().collect(
+                Collectors.summarizingInt(String::length));
+        double averageWordLength = summary.getAverage();
+        double maxWordLength = summary.getMax();
+        System.out.println("Średnia długość słów: " + averageWordLength);
+        System.out.println("Maksymalna długość słowa: " + maxWordLength);
+        System.out.println("Metoda forEach:");
+        noVowels().limit(10).forEach(System.out::println);
+    }
 }
